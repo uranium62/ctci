85376e01005fd69e0d51ac6e761509cc5e72aafc
Oleksandr Melnyk2 <oleksandr_melnyk2@epam.com>
2016-08-18 13:09:34 +0300

    Complete 1.3 1.4 exercises

--- ch1.go
+++ ch1.go
@@ -1,5 +1,6 @@
 package ctci
 
+
 func IsUnique(str string) bool {
 	if len(str) > 128 {
 		return false
@@ -26,4 +27,54 @@ func Reverse(str string) string {
 	}
 
 	return string(arr)
+}
+
+func IsPermutation(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	count := make([]int, 256)
+
+	for _, c := range str1 {
+		count[rune(c)]++
+	}
+	for _, c := range str2 {
+		ch := rune(c)
+		count[ch]--
+		if count[ch] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func ReplaceSpace(str string) string {
+	src := []byte(str)
+	length := len(src)
+	count := 0
+
+	for i := 0; i < length; i++ {
+		if src[i] == ' ' {
+			count++
+		}
+	}
+
+	des := make([]byte, length+count*2)
+
+	for i, j := 0, 0; i < length; {
+		if src[i] == ' ' {
+			des[j] = '%'
+			j++
+			des[j] = '2'
+			j++
+			des[j] = '0'
+		} else {
+			des[j] = src[i]
+		}
+		i++
+		j++
+	}
+
+	return string(des)
 }
--- ch1_test.go
+++ ch1_test.go
@@ -45,8 +45,59 @@ func TestReverse(t *testing.T) {
 		// Act
 		res := Reverse(it.input)
 
+		// Assert
 		if res != it.expect {
 			t.Errorf("Reverse: input %q, expect %q, but got %q\n", it.input, it.expect, res)
+		}
+	}
+}
+
+func TestIsPermutation(t *testing.T) {
+	// Arrange
+	cases := []struct {
+		str1   string
+		str2   string
+		expect bool
+	}{
+		{"", "", true},
+		{"", ".", false},
+		{"a", "a", true},
+		{"a", "b", false},
+		{"abc", "bac", true},
+		{"abc", "abb", false},
+	}
+
+	for _, it := range cases {
+		// Act
+		res := IsPermutation(it.str1, it.str2)
+
+		// Assert
+		if res != it.expect {
+			t.Errorf("IsPermutation: input %q %q, expect %q, but got %v\n", it.str1, it.str2, it.expect, res)
+		}
+	}
+}
+
+func TestReplaceSpace(t *testing.T) {
+	// Arrange
+	cases := []struct {
+		input  string
+		expect string
+	}{
+		{"", ""},
+		{"a", "a"},
+		{" ", "%20"},
+		{"  ", "%20%20"},
+		{" Hello world! ", "%20Hello%20world!%20"},
+	}
+
+	for _, it := range cases {
+		// Act
+		res := ReplaceSpace(it.input)
+
+		// Assert
+		if res != it.expect {
+			t.Errorf("ReplaceSpace: input %q, expect %q, but got %q\n", it.input, it.expect, res)
 		}
 	}
 }
